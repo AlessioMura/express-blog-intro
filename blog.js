e50d5720d8640a9a5451d3ecb33939e89f854925
AlessioMura <alessiomura03@gmail.com>
2024-10-30 17:02:29 +0100

    added h1 in page

--- blog.js
+++ blog.js
@@ -1,9 +1,18 @@
 //1 Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
 //2 Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
-const express = require ('express');
+const express = require('express');
 const app = express();
 const host = 'http://127.0.0.1';
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Benvenuto nel mio blog!</h1>`)
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${host}:${port}`);
+
+})
 //3 Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 //4 Creiamo poi una rotta /posts che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array locale.
 //5 La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
